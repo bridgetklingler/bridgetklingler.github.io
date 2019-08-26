da79f1db8c4dd5f83db7723e02f06200999ede30
Bridget Klingler <bridget.klingler@gmail.com>
2019-08-26 18:42:11 -0400

    styling, added more projects

--- dist/javaScript.2e490c40.js
+++ dist/javaScript.2e490c40.js
@@ -194,7 +194,7 @@ var parent = module.bundle.parent;
 if ((!parent || !parent.isParcelRequire) && typeof WebSocket !== 'undefined') {
   var hostname = "" || location.hostname;
   var protocol = location.protocol === 'https:' ? 'wss' : 'ws';
-  var ws = new WebSocket(protocol + '://' + hostname + ':' + "49258" + '/');
+  var ws = new WebSocket(protocol + '://' + hostname + ':' + "59568" + '/');
 
   ws.onmessage = function (event) {
     checkedAssets = {};
